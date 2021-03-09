1b8981c8faecfbc15dcae10dc967bc388229ea07
example <mhendisbey@gmail.com>
2021-03-10 02:57:10 +0300

    Add project files.

---
+++ CSharpIntro/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace CSharpIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer { ID = 1, FistName = "Ali", LastName = "Atalay", City = "Kahramanmaraş" };
+            Customer customer2 = new Customer(2, "Ali", "ATALAY", "Tunceli");
+
+            Customer customer3 = new Customer();
+            customer3.ID = 3;
+            customer3.FistName = "Sultan";
+            customer3.LastName = "DAVSAN";
+            customer3.City = "Sivas";
+
+            
+            Console.WriteLine(customer1.LastName);
+            Console.WriteLine(customer2.LastName);
+            Console.WriteLine(customer3.LastName);
+        }
+    }
+    class Customer
+    {
+        public Customer()
+        {
+
+        }
+        public Customer(int id,string firsName,String lastName,String city)
+        {
+            ID = id;
+            FistName = firsName;
+            LastName = lastName;
+            City = city;
+            Console.WriteLine(firsName);
+         
+        }
+        public int ID { get; set; }
+        public string FistName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> sehirler = new List<string>();
+            Console.WriteLine(sehirler.Count);
+            sehirler.Add("Nurhak");
+            Console.WriteLine(sehirler[0]);
+            Console.WriteLine(sehirler.Count);
+
+
+            MyList<String> sehirler2 = new MyList<string>();
+            Console.WriteLine(sehirler2.Count);
+            sehirler2.Add("Maraş");
+            
+            Console.WriteLine(sehirler2.Count);
+
+            foreach (var sehir in sehirler)
+            {
+                Console.WriteLine(sehir);
+            }
+        }
+        class MyList<T>  //Generic Class
+        {
+            T[] _array;
+            T[] _tempArray;
+            public MyList()
+            {
+                _array = new T[0];
+
+            }
+            public void Add(T item)
+            {
+                _tempArray  = _array;
+                _array = new T[_array.Length + 1];
+                for (int i = 0; i < _tempArray.Length; i++)
+                {
+                    _array[i] = _tempArray[i];
+                }
+                _array[_array.Length - 1] = item;
+            }
+
+            public int Count
+            {
+                get { return _array.Length; }
+              
+            }
+
+        }
+    }
+}
