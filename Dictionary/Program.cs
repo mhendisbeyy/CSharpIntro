e0cafddcf201014f33c06d2e33be96697f1d4c57
example <mhendisbey@gmail.com>
2021-03-10 03:54:08 +0300

    CSharpTemelleri

---
+++ Dictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> Ders = new Dictionary<int, string>();
+            Ders.Add(1, "Veri Tabanı");
+            Ders.Add(2, "Yazılım Mühendisliği");
+            Ders.Add(3, "Veri Yapıları");
+            Ders.Add(4, "Yapay Zeka");
+            Console.Write("Seçilen Ders : ");
+            Random rnd = new Random();
+            int ders = rnd.Next(1, 5);
+            Console.WriteLine(Ders[ders]);
+        }
+    }
+}
