7914d4c7d6ec76d07a4022bc5a420bd7daa95045
example <mhendisbey@gmail.com>
2021-03-15 19:57:59 +0300

    15.03.2021 Dama Tahtası

---
+++ DamaTahtasi/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DamaTahtasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GenerateButtons();      
+        }
+        private void GenerateButtons()
+        {
+            Button[,] buttons = new Button[8, 8];
+            int top = 10;
+            int left = 10;
+
+            for (int i = 0; i < buttons.GetUpperBound(0); i++)
+            {
+
+                for (int j = 0; j < buttons.GetUpperBound(0); j++)
+                {
+
+                    buttons[i, j] = new Button();
+                    buttons[i, j].Width = 50;
+                    buttons[i, j].Height = 50;
+                    buttons[i, j].Left = left;
+                    buttons[i, j].Top = top;
+                    left += 50;
+                    if ((i + j) % 2 == 0)
+                    {
+                        buttons[i, j].BackColor = Color.Black;
+                    }
+                    else
+                    {
+                        buttons[i, j].BackColor = Color.White;
+                    }
+                    this.Controls.Add(buttons[i, j]);
+
+
+                }
+                left = 10;
+                top += 50;
+            }
+        }
+    }
+}
