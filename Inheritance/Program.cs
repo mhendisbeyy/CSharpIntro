a67efc4dcb2b30dd66c1169918158bd81219afce
example <mhendisbey@gmail.com>
2021-03-16 01:20:08 +0300

    16.03.2021  İnheritance

---
+++ Inheritance/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] persons = new Person[3]
+            {
+                new Customer {FirstName="Ali",LastName="ATALAY",ID=1,City="Nurhak"},
+                new Student { FirstName="Hüseyin",LastName="ATALAY",ID=2,Department="Computer Science"},
+                new Person{FirstName="Ahmet",LastName="ATALAY"}
+            };
+
+            foreach (var person in persons)
+            {
+                Console.WriteLine(person.FirstName);
+            }
+
+            Console.ReadLine();
+        }
+    }
+    class Person
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+    class Person2
+    {
+
+    }
+
+    class Customer:Person
+    {
+        public string City { get; set; }
+    }
+    class Student:Person
+    {
+        public string Department { get; set; }
+    }
+}
